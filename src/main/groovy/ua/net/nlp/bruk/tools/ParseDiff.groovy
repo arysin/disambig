fdb14fd32b41962f02b1e84cf4428faf63746944
Andriy Rysin <arysin@gmail.com>
2023-04-17 10:18:46 -0400

    add ParseDiff

---
+++ src/main/groovy/ua/net/nlp/bruk/tools/ParseDiff.groovy
@@ -0,0 +1,123 @@
+#!/usr/bin/env groovy
+
+package ua.net.nlp.bruk.tools;
+
+int totalCnt = 0
+int totalWordCnt = 0
+int tokenCnt = 0
+int minusCnt = 0
+int lemmaCnt = 0
+int lemmaPosCnt = 0
+int tagCnt = 0
+Map<String, Integer> tagChg = [:].withDefault { 0 }
+Map<String, Set<Object>> lemmaChg = [:].withDefault { new HashSet<>() }
+
+List<String> benchFiles = new File("ignore_for_stats.txt").readLines().collect{ it.replace('.txt', '') }
+
+new File(".").eachFile { File f ->
+    if( ! benchFiles.find { f.name.startsWith(it) } ) {
+        return
+    }
+    
+    if( f.name ==~ /[A-I]_.*\.xml/ && ! f.name.endsWith('.tagged.xml') ) {
+        totalCnt += f.readLines().count{  it.contains("<token") }
+        totalWordCnt += f.readLines().count{  it =~ /(?iu)<token value="[а-яіїєґ]/ }
+        return
+    }
+
+    if( ! f.name.endsWith('.tagged.diff') ) {
+        return
+    }
+//    println "${f.name}"
+    
+    int line = 0
+    List<String> minuses = []
+    def lastPlus = null
+    def ctxPrev = ''
+    def ctxNext = ''
+    
+    f.eachLine{ String l ->
+        if( lastPlus ) {
+            lastPlus['ctxNext'] = l
+            lastPlus = null
+        }
+        
+        if( l =~ /^-\s*<token/ ) {
+            minuses << l
+            minusCnt++
+            lastPlus = null
+        }
+        else if( l =~ /^\+\s*<token/ ) {
+            String plus = l
+            
+            if( minuses ) {
+                String minus = minuses.remove(0)
+                def m = parse(minus)
+                def p = parse(plus)
+
+                def mm = m[1]
+                if( m[1] != p[1] ) {
+//                    println "$line: ${m[1]} -> ${p[1]}"
+//                    println "\t$m\n\t$p"
+                    lastPlus = ['minus': m, 'plus': p, 'ctxPrev': ctxPrev]
+                    lemmaChg[ "${m[1]} -> ${p[1]}" ] << lastPlus 
+                    lemmaCnt++
+                    
+                    lemmaPosCnt++
+                }
+                else {
+                    if( m[2].split(':')[0] != p[2].split(':')[0] ) {
+                        lemmaPosCnt++
+                    }
+                }
+                if( m[2] != p[2] ) {
+                    tagChg[ "${m[2]} -> ${p[2]}" ] += 1
+                    tagCnt++
+                }
+            }
+            else {
+                def p = parse(plus)
+                println "\tno match: $p"
+                lastPlus = null
+            }
+            ctxPrev = ''
+        }
+        else if( l =~ /^\s+<token/ ) {
+//            if( lastPlus ) { 
+//                lastPlus['ctxNext'] = l
+//                lastPlus = null
+//            }  
+//            else 
+                ctxPrev = l
+        }
+        else {
+            minuses = []
+            lastPlus = null
+            ctxPrev = ''
+        }
+        line++
+    }
+}
+
+println "Total -: $minusCnt (of $totalCnt - ${minusCnt*100/totalCnt}%, words: $totalWordCnt)"
+println "Lemmas: $lemmaCnt (${lemmaCnt*100d/minusCnt}%) - (${lemmaCnt*100d/totalCnt}%)"
+println "Lemma/POS: $lemmaPosCnt (${lemmaPosCnt*100d/minusCnt}%) - (${lemmaPosCnt*100d/totalCnt}%)"
+println "Tags: $tagCnt (${tagCnt*100d/minusCnt}%)"
+
+new File("zz_diff_tag.txt").text = tagChg.toSorted{ e -> -e.value }
+    .collect{ k,v -> "$k : $v" }.join("\n")
+
+println "zna/naz: " + tagChg.findAll { e -> e.key =~ /naz.*zna|zna.*naz/ }.collect { e -> e.value }.sum(0d) 
+    
+new File("zz_diff_lemma.txt").text = lemmaChg.toSorted{ e -> -e.value.size() }
+    .collect{ k,v ->
+        def vv = v.collect{ "${it.ctxPrev}\n\t${it.minus}\n\t${it.plus}\n${it.ctxNext}"}.join("\n---\n\t")
+        "$k\n\t$vv" 
+     }.join("\n")
+
+
+def parse(String l) {
+    def m = l =~ /value="(.+?)" lemma="(.+?)" tags="(.+?)"/
+    m.find()
+    [m.group(1), m.group(2), m.group(3).replaceAll(/:comp.|:&amp;predic|:(.n)?anim|:&amp:.*?:(im)?perf/,'')]
+}
